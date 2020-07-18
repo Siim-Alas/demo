ba8583450541cabb89c64d50c1d38968d808afdd
Siim Alas <siim.alas@outlook.com>
2020-07-18 12:51:44 +0300

    Changed .dzi back to .xml.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 var viewer = OpenSeadragon({
     id: "openseadragon1",
     prefixUrl: "openseadragon-bin-2.4.2/openseadragon-bin-2.4.2/images/",
-    tileSources: "talvine_maastik.dzi"
+    tileSources: "talvine_maastik.xml"
 });
 
 

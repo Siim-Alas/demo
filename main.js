f77c3b5d7b72747e954cdf6ff15755dc4998d0fc
Siim Alas <siim.alas@outlook.com>
2020-07-18 16:43:31 +0300

    Changed to image format.

--- main.js
+++ main.js
@@ -2,5 +2,8 @@
 var viewer = OpenSeadragon({
     id: "openseadragon1",
     prefixUrl: "openseadragon-bin-2.4.2/openseadragon-bin-2.4.2/images/",
-    tileSources: "Images/talvine_maastik/talvine_maastik.xml"
+    tileSources: {
+        type: 'image',
+        url: 'Taxus_uitgezaaid_in_een_knotwilg._28-07-2019._(d.j.b)._08.jpg'
+    }
 });

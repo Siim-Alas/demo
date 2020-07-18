3574d8e14d516b762b98e692c77324729f96ea41
Siim Alas <siim.alas@outlook.com>
2020-07-18 12:57:49 +0300

    Redid folder structure.

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
 
-
 var viewer = OpenSeadragon({
     id: "openseadragon1",
     prefixUrl: "openseadragon-bin-2.4.2/openseadragon-bin-2.4.2/images/",
-    tileSources: "talvine_maastik.xml"
+    tileSources: "Images/talvine_maastik/talvine_maastik.xml"
 });
-
-
-
-
-//fileInput.addEventListener("change", function () {
-//    console.log(this.files);
-//    if (this.files != null && this.files[0] != null) {
-//        let file = this.files[0];
-//
-//        let formData = new FormData();
-//        formData.append("image", file);
-//
-//        fetch('https://srv2.zoomable.ca/api/image', {
-//            method: 'POST',
-//            mode: 'no-cors',
-//            body: formData,
-//        }).then(function (response) {
-//            console.log(response.json());
-//        });
-//    }
-//});

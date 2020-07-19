f5bf614628754e37ead5da02cdf2b73a0674d4d7
Siim Alas <siim.alas@outlook.com>
2020-07-19 16:19:04 +0300

    Changed immage.

---
+++ Javascript/Own/main.js
@@ -0,0 +1,29 @@
+
+var viewer = OpenSeadragon({
+    id: "openseadragon1",
+    prefixUrl: "Javascript/dist/openseadragon-bin-2.4.2/images/",
+    tileSources: 'Images/output/Bernardo_Bellotto,_il_Canaletto_-_View_of_Vienna_from_the_Belvedere.xml'
+});
+
+document.getElementById("fileInput").addEventListener("change", function () {
+    if (this.files != null && this.files[0] != null) {
+        let _this = this;
+        buildTilePyramidAndXML(
+            this.files[0],
+            function (xml, tiles) {
+                let zip = new JSZip();
+
+                zip.file(`${_this.files[0].name.substring(0, _this.files[0].name.lastIndexOf('.'))}.xml`, xml);
+
+                for (t of tiles) {
+                    zip.file(t.name, t.canvas.toDataURL().split('base64,')[1], { base64: true });
+                }
+
+                zip.generateAsync({ type: "blob" }).then(function (blob) {
+                    saveAs(blob, "output.zip");
+                });
+
+                console.log(tiles);
+        }, 256);
+    }
+});

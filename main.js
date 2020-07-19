28eb45dd482f2f636433ba228ad6f2b35a6ed5d4
Siim Alas <siim.alas@outlook.com>
2020-07-19 13:25:17 +0300

    Fixed typo.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var viewer = OpenSeadragon({
     id: "openseadragon1",
     prefixUrl: "openseadragon-bin-2.4.2/openseadragon-bin-2.4.2/images/",
-    tileSources: 'Images/profiilipilt/talvine_maastik/talvine_maastik.xml'
+    tileSources: 'Images/talvine_maastik/talvine_maastik.xml'
 });
 
 document.getElementById("fileInput").addEventListener("change", function () {

fc83dabc8e97c4acaf5d1c79335a639857b0a3e3
Siim Alas <siim.alas@outlook.com>
2020-07-19 08:43:17 +0300

    Started implementing own render pyramid builder.

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ var viewer = OpenSeadragon({
         url: 'Taxus_uitgezaaid_in_een_knotwilg._28-07-2019._(d.j.b)._08.jpg'
     }
 });
+
+document.getElementById("fileInput").addEventListener("change", function () {
+    if (this.files != null && this.files[0] != null) {
+        let img;
+        let levels;
+
+        let reader = new FileReader();
+        reader.onload = function () {
+            img = document.createElement('img');
+            img.src = reader.result;
+            img.onload = function () {
+                levels = buildLevels(img);
+
+                console.log(levels);
+            };
+        };
+
+        reader.readAsDataURL(this.files[0]);
+    }
+});

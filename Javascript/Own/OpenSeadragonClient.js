7efa725c9a90d860ef6afe920678f5fde71d671d
Siim Alas <siim.alas@outlook.com>
2020-07-23 19:08:57 +0300

    Attempted Blazor deployment.

--- Javascript/Own/OpenSeadragonClient.js
+++ Javascript/Own/OpenSeadragonClient.js
@@ -1,24 +1,14 @@
 
-window.addEventListener("load", function () {
-    let viewer = OpenSeadragon({
-        id: "openseadragon1",
-        prefixUrl: "Javascript/dist/openseadragon-bin-2.4.2/images/",
-        tileSources: 'Images/output/2196386940.xml'
-    });
+fetch("https://siim-alas.github.io/demo/Images").then(r => {
+    console.log(r);
+});
 
-    document.getElementById("updateViewerFileInput").addEventListener("change", function () {
-        if (this.files != null && this.files[0] != null) {
-            let reader = new FileReader();
-            reader.onload = function () {
-                viewer.open({
-                    type: 'image',
-                    url: reader.result,
-                    crossOriginPolicy: 'Anonymous',
-                    ajaxWithCredentials: false
-                }
-                );
-            }
-            reader.readAsDataURL(this.files[0]);
-        }
-    });
-})
+window.OpenSeadragonClient = {
+    initDZI: function (viewerElement, tileSourcePath) {
+        OpenSeadragon({
+            element: viewerElement,
+            prefixUrl: "Javascript/dist/openseadragon-bin-2.4.2/images/",
+            tileSources: tileSourcePath
+        });
+    }
+}
